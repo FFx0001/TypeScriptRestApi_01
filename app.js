ddbaeeec223d0e2138a400c2c19d51b7e6f8494d
FFx0001 <vip.ffx0001@mail.ru>
2020-03-16 21:59:08 +0700

    Init: add mongodb connection and starting settings to default server.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("APP: [S T A R T I N G] ...");
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+const app = express();
+const LPORT = config.get('LPort') || 5000;
+async  function start(){
+    try{
+         await mongoose.connect(config.get('MongoURI'),{
+             useNewUrlParser:true,
+             useUnifiedTopology:true,
+             useCreateIndex:true
+         });
+         app.listen(LPORT,()=>console.log(`App has been started on port: ${LPORT}`));
+    }catch (e) {
+        console.log('Server Error',e.message)
+        process.exit(1);
+    }
+}
+start();
